672123f72f5a4c1228bcfb9b1c269fb86a17efc5
havaian <m.usman.work@gmail.com>
2025-10-30 17:15:01 +0500

    Some design updates

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -54,16 +54,7 @@
             <!-- Auth Buttons -->
             <div v-if="!authStore.isAuthenticated" class="flex items-center space-x-4">
               <button @click="authStore.login" class="btn-auth inline-flex items-center">
-                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 24 24">
-                  <path
-                    d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
-                  <path
-                    d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
-                  <path
-                    d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" />
-                  <path
-                    d="M12 1c2.97 0 5.46.98 7.28 2.66l-3.57 2.77c-.98-.66-2.23-1.06-3.71 1.06-2.86 0-5.29 1.93-6.16 4.53H2.18V8.07C3.99 3.47 7.7 1 12 1z" />
-                </svg>
+                <GoogleIcon size="16" className="mr-2" />
                 {{ $t('auth.signInWithGoogle') }}
               </button>
             </div>
@@ -504,6 +495,7 @@ import { ref, computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import GoogleIcon from '@/components/icons/GoogleIcon.vue'
 import {
   ChevronDownIcon,
   Bars3Icon,
